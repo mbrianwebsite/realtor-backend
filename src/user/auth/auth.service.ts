530422b6b77c7af2b08b9b657aeb25bd94735aa9
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-03 08:56:36 +0700

    SAVE SIGNUP DATA TO DATABASE

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcript from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 interface SignupParams {
   email: string;
@@ -12,17 +13,29 @@ interface SignupParams {
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  async signup({ email, password }: SignupParams) {
+  async signup({ email, password, name, phone }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email,
       },
     });
 
-    // if (userExists) {
-    //   throw new ConflictException();
-    // }
+    if (userExists) {
+      throw new ConflictException();
+    }
 
     const hashedPassword = await bcript.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
