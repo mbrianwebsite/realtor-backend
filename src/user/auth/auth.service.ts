e7501ce7829c2d617888f6dc36f38aeb05618076
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-03 08:14:23 +0700

    SET UP VALIDATE REGISTER INPUT AND EMAIL EXIST

---
+++ src/user/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+  }
+}
