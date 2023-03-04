af3c4ddf7c7352dbadb31352d6f0f19b2c5be3a1
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-04 21:12:22 +0700

    RETURN HOME DATA WITH RELATIONSHIP

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -7,7 +7,25 @@ export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getHomes(): Promise<HomeResponseDto[]> {
-    const homes = await this.prismaService.home.findMany();
-    return homes.map((home) => new HomeResponseDto(home));
+    const homes = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        propertyType: true,
+        number_of_bedrooms: true,
+        number_of_bathrooms: true,
+        images: {
+          select: { url: true },
+          take: 1,
+        },
+      },
+    });
+    return homes.map((home) => {
+      const fetchHome = { ...home, image: home.images[0].url };
+      delete fetchHome.images;
+      return new HomeResponseDto(fetchHome);
+    });
   }
 }
