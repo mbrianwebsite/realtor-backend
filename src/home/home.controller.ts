95edf3ef70975841d49a0783696fa5ed4c9587b9
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-04 20:59:41 +0700

    GET ALL HOME API

---
+++ src/home/home.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { HomeResponseDto } from './dto/home.dto';
+import { HomeService } from './home.service';
+
+@Controller('home')
+export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+
+  @Get()
+  getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
+  }
+
+  @Get(':id')
+  getHome() {
+    return {};
+  }
+
+  @Post()
+  createHome() {
+    return {};
+  }
+
+  @Put(':id')
+  updateHome() {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteHome() {
+    return;
+  }
+}
