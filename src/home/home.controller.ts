50634ef2f7fc287044b5e41139d80830d4801fff
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-04 21:48:26 +0700

    SET UP QUERY FILTER

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
+import { PropertyType } from '@prisma/client';
 import { HomeResponseDto } from './dto/home.dto';
 import { HomeService } from './home.service';
 
@@ -7,7 +8,13 @@ export class HomeController {
   constructor(private readonly homeService: HomeService) {}
 
   @Get()
-  getHomes(): Promise<HomeResponseDto[]> {
+  getHomes(
+    @Query('city') city?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('propertyType') propertyType?: PropertyType,
+  ): Promise<HomeResponseDto[]> {
+    console.log({ city, minPrice, maxPrice, propertyType });
     return this.homeService.getHomes();
   }
 
