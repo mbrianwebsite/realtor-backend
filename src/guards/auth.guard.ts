2e5154e1e9dd6733896c8d5796c61cc388242b46
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-06 08:00:47 +0700

    SET UP VERIFY JWT TOKEN

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,14 +1,27 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.getAllAndOverride('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
+
+    if (roles?.length) {
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers?.authorization?.split('Bearer ')[1];
+      try {
+        const user = await jwt.verify(token, process.env.JSON_TOKEN_KEY);
+        console.log({ user });
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
 
     console.log(roles);
 
